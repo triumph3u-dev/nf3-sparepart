e3ac399b9287d57df559332af00f361ab21cde77
triumph3u-dev <triumph.3u@gmail.com>
2026-07-24 15:56:40 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * เปลี่ยนเลข CACHE ทุกครั้งที่แก้ index.html ไม่งั้นเครื่องที่ติดตั้งไว้แล้ว
  * จะยังใช้ไฟล์เก่าค้างอยู่
  */
-const CACHE = "nf3-sp-v7";
+const CACHE = "nf3-sp-v8";
 
 const SHELL = [
   "./",
